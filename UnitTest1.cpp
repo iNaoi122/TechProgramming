83061d5216c0dc1fd4860aae1a8fde7201ad75d6
MS-legend <145836942+MS-legend@users.noreply.github.com>
2024-05-30 20:33:57 +0300

    Add files via upload

---
+++ UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int a = 5;
+			int b = 3;
+			int c = 2;
+
+			Assert::AreEqual(a, b + c);
+			
+
+		}
+	};
+}
