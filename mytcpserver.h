ff37f5152c6b5c4560ae60d5d7c3ba468f255983
iNaoi122 <88289995+iNaoi122@users.noreply.github.com>
2024-06-16 16:21:25 +0300

    Add files via upload

--- mytcpserver.h
+++ mytcpserver.h
@@ -1,33 +1,42 @@
 #ifndef MYTCPSERVER_H
 #define MYTCPSERVER_H
+
 #include <QObject>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include "server_logic.h"
 
-#include <QtNetwork>
-#include <QByteArray>
-#include <QDebug>
+/*!
+ * \brief The MyTcpServer class
+ * \author Сергей Анциферов
+ */
 
 class MyTcpServer : public QObject
 {
     Q_OBJECT
 public:
-    explicit MyTcpServer(QObject *parent = nullptr);
-    ~MyTcpServer();
+    /*!
+     * \brief MyTcpServer
+     * \param parent
+     */
+    explicit MyTcpServer(QObject *parent = 0);
+     ServerLogic logic;
+
 public slots:
+     /*!
+     * \brief slotNewConnection
+     */
     void slotNewConnection();
-    void slotClientDisconnected();
     void slotServerRead();
+    void slotClientDisconnected();
+
 private:
+    QList<QTcpSocket*> clients;
     QTcpServer * mTcpServer;
     QTcpSocket * mTcpSocket;
-    //int server_status;
+    QString wait_request(QTcpSocket *socket);
+
 };
+
+
 #endif // MYTCPSERVER_H
-
-
-
-
-
-
-
